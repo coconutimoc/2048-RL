b22fe354fdba055b930d34c94dd5d6c00ea62747
coconutboi <coconutimoc@gmail.com>
2024-10-26 17:26:17 +0800

    rewrite the kernal and gui,which fixes #4

--- 2048_gui.py
+++ 2048_gui.py
@@ -1,119 +1,74 @@
+from kernal import game2048
 import tkinter as tk
-import random
-import matrix as mk
-import math
-import csv
-import time
+import json
+from datetime import datetime
 
-class Game2048:
-    def __init__(self, master, dimension=4):
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+    for dict in config:
+        if dict['task'] == "gui":
+            config = dict
+            break
+
+
+class GUI:
+    def __init__(self, master):
+        #config the window
         self.master = master
-        self.master.title('2048 Game')
-        self.master.geometry('600x600')
-        self.master.resizable(0, 0)
-        self.grid_size = dimension
-        self.cell_size = 600 // self.grid_size
+        self.master.title(config["window"]["title"])
+        self.master.geometry(config["window"]["geometry"])
+        self.master.bind('<Key>', self.key_pressed)
+        self.master.config(bg=config["window"]["bg"])
+
+        #initialize the game
+        self.game = game2048(dimension=config['dimension'])
+        
+        #initialize the UI
+        self.score_label = tk.Label(self.master, text=f'Score:{self.game.score}', font=(config["cell"]["font"]["family"], config["cell"]["font"]["size"],\
+                 config["cell"]["font"]["weight"])).grid(row=0, column=0, columnspan=self.game.dimension)
         self.cells = []
-        self.matrix = mk.matrix(self.grid_size)
-        self.init_grid()
-        self.master.bind("<Key>", self.key_handler)
+        for line in range(0, self.game.dimension):
+            for column in range(0, self.game.dimension):
+                cell = tk.Label(self.master, text='', font=(config["cell"]["font"]["family"], config["cell"]["font"]["size"],\
+                 config["cell"]["font"]["weight"]), width=config["cell"]["width"], height=config["cell"]["width"], bg=config["cell"]["color"]["0"])
+                cell.grid(row=line+1, column=column)
+                self.cells.append(cell)
+        self.update_ui()
 
-    def init_grid(self):
-        background = tk.Frame(self.master, bg='azure3', width=self.grid_size * self.cell_size, height=self.grid_size * self.cell_size)
-        background.grid(pady=(100, 0))
-        for i in range(self.grid_size):
-            row = []
-            for j in range(self.grid_size):
-                cell = tk.Frame(background, bg='azure4', width=self.cell_size, height=self.cell_size)
-                cell.grid(row=i, column=j, padx=5, pady=5)
-                t = tk.Label(master=cell, text='', bg='azure4', justify=tk.CENTER, font=('arial', 22, 'bold'), width=4, height=2)
-                t.grid()
-                row.append(t)
-            self.cells.append(row)
-        self.update_grid()  
-
-    def update_grid(self):
-        for i in range(self.grid_size):
-            for j in range(self.grid_size):
-                value = self.matrix.matrix[i][j]
+    def update_ui(self):
+        for i in range(0, self.game.dimension):
+            for j in range(0, self.game.dimension):
+                index = i * self.game.dimension + j
+                value = self.game.matrix[i][j]
                 if value == 0:
-                    self.cells[i][j].configure(text='', bg='azure4')
+                    self.cells[index].config(text='', bg='gray')
                 else:
-                    color = "#{:02x}{:02x}{:02x}".format(255 - int(math.log(value, 2) * 30), 255 - int(math.log(value, 2) * 30), 255)
-                    self.cells[i][j].configure(text=str(int(value)), bg=color)
-
-    def key_handler(self, event):
-        key = event.keysym
-        if key in ('Up', 'Down', 'Left', 'Right'):
-            self.matrix.update(key.lower())
-            self.update_grid()
-            if self.matrix.gameover():
+                    self.cells[index].config(text=str(int(value)), bg=config["cell"]['colors'][str(value)])
+        self.score_label.config(text=f'Score:{self.game.score}')
+    
+    def key_pressed(self, event):
+        if config["key"][event.keysym] in ['up', 'down', 'left', 'right']:
+            self.game.update(config["key"][event.keysym])
+            self.update_ui()
+            if self.game.gameover():
                 self.game_over()
-        if key == '<ctrl-q>':
+        if config["key"][event.keysym] == 'quit':
             self.master.quit()
-            print('Successfully quit! No score will be recorded.')
-
+    
     def game_over(self):
-        game_over_frame = tk.Frame(self.master, borderwidth=2)
-        game_over_frame.place(relx=0.5, rely=0.5, anchor="center")
-        tk.Label(game_over_frame, text="Game Over!", bg='red', fg='white', font=('arial', 22, 'bold')).pack()
-        print('Game Over! Your score is:', self.matrix.score)
-        if globals().get('username') is not None:
-                with open('.dict/log.csv', 'a') as log:
-                    writer = csv.writer(log)
-                    writer.writerow([username, self.matrix.score, self.matrix.maxnum,self.matrix.dimension,time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())])
+        self.game_over_label = tk.Label(self.master, text='Game Over!', font=('Arial', 40, 'bold'), bg='red')
+        self.game_over_label.grid(row=0, column=0, columnspan=self.game.dimension)
+        self.master.unbind('<Key>')
+        self.master.bind('<Key>', self.quit)
+        with open('log.csv', 'a+') as log:
+            log.write(f'{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}, config["username"], {self.game.dimension},{self.game.score}\n')
+        
+    def quit(self, event):
+        if config["key"][event.keysym] == 'quit':
+            self.master.quit()
 
-if __name__ == "__main__":
-    print("----------------------------------")
-    print('Welcome to 2048!')
-    print('Author: Coconutboi')
-    print('Version: 0.1')
-    print('mode: GUI')
-    print('----------------------------------')
-    print()
-
-
-    with open('.dict/user.csv', 'a+') as user_file:
-        users = csv.reader(user_file)
-        writer = csv.writer(user_file)
-        print('----------------------------------')
-        if input ('Do you want to login?[y/n]: ') in ['y', 'Y']:
-            while True:
-                username = input('Username: ')
-                if users is not None:
-                    for user in users:
-                        if user[0] == username:
-                            correct_password = user[1]
-                if globals().get('correct_password') is not None:
-                    while True:
-                        password = input('Password: ')
-                        if password == correct_password:
-                            print('Welcome back,', username)
-                            break
-                        else:
-                            print('Password incorrect! Please try again.')
-                            continue
-
-                else:
-                    if input('User not found! Do you want to create a new account?[y/n]:') in ['y', 'Y']:
-                        while True:
-                            username = input('Username: ')
-                            password = input('Password: ')
-                            if password == input('Confirm password: '):
-                                writer.writerow([username, password])
-                                print('Account created successfully!')
-                                break
-                            else:
-                                print('Password not match! Please try again.')
-                        break
-                    else:
-                        print('Please try again.')
-                        continue
-        else:
-            print('Now you are playing as a guest.')
-    print()
-    dimension = int(input('Dimension: '))
-
+if __name__ == '__main__':
     root = tk.Tk()
-    game = Game2048(root,dimension)
+    gui = GUI(root)
     root.mainloop()
+    
